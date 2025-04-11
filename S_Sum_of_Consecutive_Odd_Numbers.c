949e0a6040549baaa0846b94f5291b2912dbd3aa
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-11 22:18:33 +0600

    S. Sum of Consecutive Odd Numbers problem solved

--- S_Sum_of_Consecutive_Odd_Numbers.c
+++ S_Sum_of_Consecutive_Odd_Numbers.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 int main()
 {
+    int T;
+    scanf("%d",&T);
+    for (int tc = 1; tc <=T; tc++)
+    {
+        
     int sum = 0;
     int X, Y;
     scanf("%d %d", &X, &Y);
-    if (Y > X)
+    if (X > Y)
     {
-        for (int i = X + 1; i < Y; i++)
+        int temp = Y;
+        Y = X;
+        X = temp;
+    }
+    for (int i = X + 1; i < Y; i++)
+    {
+        if (i % 2 != 0)
         {
-            if (i % 2 != 0)
-            {
 
-                sum += i;
-            }
+            sum += i;
         }
     }
-    else if (X > Y)
-    {
-        for (int i = Y + 1; i < X; i++)
-        {
-            if (i % 2 != 0)
-            {
-
-                sum += i;
-            }
-        }
-    }
+    
 
     printf("%d\n",sum);
+    }
+    
 
     return 0;
 }
