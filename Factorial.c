59e96f263b11315de42851e3a0c76ad36d6382a4
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-12 19:34:51 +0600

    problem solution added and solved

--- Factorial.c
+++ Factorial.c
@@ -1,17 +1,22 @@
-#include<stdio.h>
-int main(){
-   
-        int N ;
-        scanf("%d",&N);
-        int factorial = 1;
-        for (int i = 1; i < N; i++)
-        {
-            // printf("%d\n",i);
-            factorial= N * i;
-            printf("%d\n",factorial);
+//https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/G
 
+#include <stdio.h>
+int main()
+{
+    int T;
+    scanf("%d", &T);
+
+        for (int tc = 1; tc <= T; tc++)
+    {
+        int N;
+        scanf("%d", &N);
+        long long int factorial = 1;
+        for (int i = 1; i <= N; i++)
+        {
+            factorial = factorial * i;
         }
-    
-    
+        printf("%lld\n", factorial);
+    }
+
     return 0;
 }
