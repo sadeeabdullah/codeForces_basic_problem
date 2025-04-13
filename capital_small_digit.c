19fe1d844c887a522806c72007ebb38d6b5e0067
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-13 10:34:24 -0500

    solution added of  the problem of capital small or digit

--- capital_small_digit.c
+++ capital_small_digit.c
@@ -4,6 +4,20 @@
 int main(){
     char X;
     scanf("%c",&X);
-    printf("%c",X);
+
+    if (X>='0' && X<='9')
+    {
+        printf("IS DIGIT");
+    }
+    else{
+        printf("ALPHA\n");
+        if (X>='A'&& X<='Z')
+        {
+            printf("IS CAPITAL\n");
+        }else{
+            printf("IS SMALL\n");
+        }
+        
+    }
     return 0;
 }
