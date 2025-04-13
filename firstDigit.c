6b4e0d2700b4f0a09a88f812c978a60fc6087567
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-13 09:16:04 -0500

    first digit even odd problem solution added

---
+++ firstDigit.c
@@ -0,0 +1,20 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/P
+
+#include<stdio.h>
+int main(){
+    int X;
+    scanf("%d",&X);
+
+    while (X >= 10)
+    {
+        X = X/10;
+    }
+    if (X % 2 == 0)
+    {
+        printf("EVEN");
+    }else{
+        printf("ODD");
+    }
+    
+    return 0;
+}
